c301a667602c868b336bb039a63fc9d9b3d8ab68
VoidCyborg <dan-27@mail.ru>
2022-12-07 16:37:37 +0200

    Пишу настройки библиотеки, а точнее настройки Appender' ов.

--- src/main/java/ru/voidcyborg/logapi/appender/Appender.java
+++ src/main/java/ru/voidcyborg/logapi/appender/Appender.java
@@ -1,7 +1,9 @@
 package ru.voidcyborg.logapi.appender;
 
+import java.util.HashMap;
+
 public interface Appender {
-    void parseSettings(String params);
+    void parseSettings(HashMap<String, String> settings);
 
     boolean append(String text);
 }
--- src/main/java/ru/voidcyborg/logapi/settings/Settings.java
+++ src/main/java/ru/voidcyborg/logapi/settings/Settings.java
@@ -1,9 +1,53 @@
 package ru.voidcyborg.logapi.settings;
 
-public class Settings {
+import ru.voidcyborg.logapi.appender.Appender;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public final class Settings {
+
+    private final String[] args;
+
+    private final Appender[] appenders;
+    private final HashMap<String, String> appenderSettings = new HashMap<>();
+
+    public Settings(String[] args) throws SettingsInitException {
+        if (args == null) throw new SettingsInitException("Parameters can't be null");
+        this.args = args.clone();
+        this.appenders = createAppenders();
+        this.parseArguments();
+    }
 
 
-    public Settings(String[] params){
+    private Appender[] createAppenders() throws SettingsInitException {
+        List<String> paths = new ArrayList<>();
+        for (String s : this.args) {
+            if (s == null) continue;
+            if (!s.startsWith("appender=")) continue;
+            String[] parts = s.split("=");
+            if (parts.length != 2) throw new SettingsInitException("Wrong settings syntax path:" + s);
+
+            paths.add(parts[1]);
+        }
+
+        if (paths.isEmpty()) throw new SettingsInitException("No single path to the Appender class.");
+
+        List<Appender> appenders = new ArrayList<>();
+        for (String s : paths) {
+            try {
+                Appender appender = (Appender) Class.forName(s).getDeclaredConstructor().newInstance();
+                appenders.add(appender);
+            } catch (Exception e) {
+                throw new SettingsInitException("Failed to create an Appender:" + s);
+            }
+        }
+
+        return appenders.toArray(new Appender[0]);
+    }
+
+    private void parseArguments() throws SettingsInitException {
 
     }
 }
