cd7ec3bade8af5c6c5213763a72d6de1212c7bf0
VoidCyborg <dan-27@mail.ru>
2022-12-12 00:36:17 +0200

    Делаю по чуть чуть.

--- src/main/java/ru/voidcyborg/logapi/appender/appenders/RollingFileAppender.java
+++ src/main/java/ru/voidcyborg/logapi/appender/appenders/RollingFileAppender.java
@@ -2,22 +2,35 @@ package ru.voidcyborg.logapi.appender.appenders;
 
 import ru.voidcyborg.logapi.appender.Appender;
 
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RollingFileAppender implements Appender {//TODO
 
 
-    private final static String PID = String.valueOf(ProcessHandle.current().pid());
+    private static final String PID = ProcessHandle.current().pid() + "-";
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     private volatile String name = "log";
     private volatile String type = "";
+    private volatile Path path = Path.of("");
 
     private volatile int maxSize = 1024_000;
     private volatile int maxFiles = -1;
 
+    private volatile int index = 0;
+
 
     @Override
-    public void parseSettings(HashMap<String, String> settings) {
+    public synchronized void parseSettings(HashMap<String, String> settings) {
         if (settings == null) return;
 
         String[] nameType = separateNameAndType(settings.get("fileName"));
@@ -25,8 +38,41 @@ public class RollingFileAppender implements Appender {//TODO
         type = nameType[1];
         maxSize = parseSize(settings.get("maxFileSize"));
         maxFiles = parseMaxFiles(settings.get("maxFiles"));
+
     }
 
+    @Override
+    public synchronized boolean append(String text) {
+        if (text == null) return false;
+
+
+        while (true) {
+            try (FileChannel channel = FileChannel.open(this.path.resolve(name + "-" + index + type), StandardOpenOption.CREATE_NEW, StandardOpenOption.APPEND)) {
+                ByteBuffer buffer = ByteBuffer.wrap(text.getBytes(StandardCharsets.UTF_8));
+                if (channel.size() + buffer.remaining() > maxSize) {
+                    index++;
+                }
+            } catch (Exception ignore) {
+                break;
+            }
+            if (maxFiles>0){
+                if(index>=maxFiles) {
+                    Files.delete();
+                }
+            }
+        }
+
+
+        return true;//TODO
+    }
+
+    private Path parsePath(String s) {
+        try {
+            return Path.of(s);
+        } catch (Exception ignore) {
+        }
+        return Path.of("");
+    }
 
     private int parseMaxFiles(String s) {
         int files = -1;
@@ -82,10 +128,5 @@ public class RollingFileAppender implements Appender {//TODO
         if (result[1].length() <= 1) result[1] = "";
 
         return result;
-    }
-
-    @Override
-    public boolean append(String text) {
-        return true;//TODO
     }
 }
