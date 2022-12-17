1ad6ae22c195d25010cb68bc5ffbfb99f925db8c
VoidCyborg <dan-27@mail.ru>
2022-12-17 17:51:39 +0200

    Делаю документацию.

--- src/main/java/ru/voidcyborg/logapi/level/LogLevel.java
+++ src/main/java/ru/voidcyborg/logapi/level/LogLevel.java
@@ -1,5 +1,22 @@
 package ru.voidcyborg.logapi.level;
 
+/**
+ * Данный enum предназначен для регулировки уровня логирования.
+ * Он состоит из 8 значений от ALL до OFF.
+ * <p>
+ *
+ * This enum is designed to adjust the logging level.
+ * It consists of 8 values from ALL to OFF.
+ * <p>
+ *
+ *
+ * @author  VoidCyborg
+ * @see     ru.voidcyborg.logapi.logger.LoggerFactory
+ * @see     ru.voidcyborg.logapi.logger.LoggerGroup
+ * @see     ru.voidcyborg.logapi.logger.Logger
+ * @see     java.util.Map
+ * @see     java.lang.String
+ */
 public enum LogLevel {
     ALL,
     TRACE,
@@ -10,9 +27,29 @@ public enum LogLevel {
     FATAL,
     OFF;
 
+
+    //Храню посчитаную строку, чтобы не считать её каждый раз при вызове .toString();
     private final String toString = calcString();
 
+    /**
+     * Данный метод предназначен для представления уровня в виде строки.
+     * Если размер строки менее 5 символов, то добавляются пробелы.
+     * Это необходимо для того, чтобы уровень занимал одинаковую длинну в логах.
+     * <p>
+     * This method is designed to represent the level as a string.
+     * If the string is less than 5 characters, spaces are added.
+     * This is necessary so that the level takes the same length in the logs.
+     * <p>
+     *
+     * @return Возвращает строку длинной 5. Returns a string with a length of 5.
+     *
+     */
+    @Override
+    public String toString() {
+        return toString;
+    }
 
+    //Высчитывает строку из имени + пробелы
     private String calcString() {
         StringBuilder result = new StringBuilder(this.name());
         if (result.length() == 5) return result.toString();
@@ -22,8 +59,5 @@ public enum LogLevel {
         return result.toString();
     }
 
-    @Override
-    public String toString() {
-        return toString;
-    }
+
 }
--- src/main/java/ru/voidcyborg/logapi/settings/SettingsInitException.java
+++ src/main/java/ru/voidcyborg/logapi/settings/SettingsInitException.java
@@ -1,10 +1,34 @@
 package ru.voidcyborg.logapi.settings;
 
+/**
+ * Класс {@code SettingsInitException} предствляет из себя ошибку инициализации настроек.
+ * <p>
+ * The {@code SettingsInitException} class represents an error initializing the settings.
+ * <p>
+ *
+ * @author  VoidCyborg
+ * @see     ru.voidcyborg.logapi.settings.Settings
+ * @see     ru.voidcyborg.logapi.logger.LoggerFactory
+ * @see     java.lang.String
+ */
 public final class SettingsInitException extends Exception {
+
+    /**
+     * Реализую стандартный конструктор для класса {@code Exception}.
+     * <p>
+     * Implement the standard constructor for the class {@code Exception}.
+     */
     public SettingsInitException() {
         super();
     }
 
+    /**
+     * Реализую стандартный конструктор для класса {@code Exception}.
+     * Рекомендуется не передавать null значение.
+     * <p>
+     * Implement the standard constructor for the class {@code Exception}.
+     * It is recommended not to pass null value.
+     */
     public SettingsInitException(String s) {
         super(s);
     }
