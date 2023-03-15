26057c3f4b0acb174e1c9c7db45af143be694dce
VoidCyborg <dan-27@mail.ru>
2023-03-15 12:37:08 +0200

    Добавил данные module-info.java для работы в javafx приложении.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module ru.voidcyborg.logapi {
+
+    exports ru.voidcyborg.logapi.appender;
+    exports ru.voidcyborg.logapi.appender.appenders;
+    exports ru.voidcyborg.logapi.level;
+    exports ru.voidcyborg.logapi.logger;
+    exports ru.voidcyborg.logapi.settings;
+}
